8fe4523a41e1fc69df21ab703df6f557516fd544
seodongmun <sdm0875@naver.com>
2024-06-17 17:35:58 +0900

    24/06/17

--- 3_JavaScript/실습문제풀이/실습문제풀이8.js
+++ 3_JavaScript/실습문제풀이/실습문제풀이8.js
@@ -1,40 +1,69 @@
-/*
-    사용자가 입력을 했을 때 
-    해당 조건이 틀릴 경우 빨간색으로 표시
-               맞는 경우 초록색 표시와 함께 OK!
-*/
 const userId = document.querySelector("#userId");
 const userIdSpan = document.querySelector("#userIdSpan");
+const userPwd = document.querySelector("#userPwd");
+const userPwdSpan = document.querySelector("#userPwdSpan");
+const userPwdCheck = document.querySelector("#userPwdCheck");
+const userPwdCheckSpan = document.querySelector("#userPwdCheckSpan");
+const userName = document.querySelector("#userName");
+const userNameSpan = document.querySelector("#userNameSpan");
+const userEmail = document.querySelector("#userEmail");
+const userEmailSpan = document.querySelector("#userEmailSpan");
 
-userId.addEventListener("input", function (e) {
-  // 첫글자는 반드시 영문자로, 그리고 영문자, 숫자 포함하여 총 4~12자로 입력하시오.
+userId.addEventListener("input", function () {
   const regExp = /^[a-zA-Z][a-zA-Z0-9]{3,11}$/;
   const check = regExp.test(userId.value);
-
   if (check) {
     userIdSpan.style.color = "green";
-    userIdSpan.innerHTML = "OK!";
-    //e.target.nextElementSibling.style.color = "green";
-    //e.target.nextElementSibling.innerHTML = "OK!";
+    userIdSpan.innerHTML = "OK";
   } else {
     userIdSpan.style.color = "red";
-    userIdSpan.innerHTML =
-      "첫글자는 반드시 영문자로, 그리고 영문자, 숫자 포함하여 총 4~12자로 입력하시오.";
-    // e.target.nextElementSibling.style.color = "red";
-    // e.target.nextElementSibling.innerHTML =
-    //  "첫글자는 반드시 영문자로, 그리고 영문자, 숫자 포함하여 총 4~12자로 입력하시오.";
+    userIdSpan.innerHTML = "...";
   }
 });
 
-// 영문자, 숫자, 특수문자 포함하여 총 8~15자로 입력하시오.
-// [!-~]{여기는 여러분들이 채워주세요!}
+userPwd.addEventListener("input", function () {
+  const regExp = /^[!-~]{7,14}$/;
+  const check = regExp.test(userPwd.value);
+  if (check) {
+    userPwdSpan.style.color = "green";
+    userPwdSpan.innerHTML = "OK";
+  } else {
+    userPwdSpan.style.color = "red";
+    userPwdSpan.innerHTML =
+      "영문자, 숫자, 특수문자 포함하여 총 8~15자로 입력하시오.";
+  }
+});
 
-// 위의 비밀번호와 일치하게 입력하시오.
-// 정규표현식 필요 X, 위에 입력한 비밀번호 값이랑 비교!
+userPwdCheck.addEventListener("input", function () {
+  if (userPwd.value === userPwdCheck.value) {
+    userPwdCheckSpan.style.color = "green";
+    userPwdCheckSpan.innerHTML = "OK";
+  } else {
+    userPwdCheckSpan.style.color = "red";
+    userPwdCheckSpan.innerHTML = "위의 비밀번호와 같게 입력하세요";
+  }
+});
 
-// 한글로만 이루어져야되며 2글자 이상으로 입력하시오.
-// [가-힣]{여기도 여러분들이 채워주세요!}
+userName.addEventListener("input", function () {
+  const regExp = /^[가-힣]{2,}$/;
+  const check = regExp.test(userName.value);
+  if (check) {
+    userNameSpan.style.color = "green";
+    userNameSpan.innerHTML = "OK";
+  } else {
+    userNameSpan.style.color = "red";
+    userNameSpan.innerHTML = "한글로 최소 2글자 이상 입력하세요";
+  }
+});
 
-// 이메일 형식에 맞춰서 입력하시오.
-// abc123!@gmail.com
-// -> (영어나숫자여러문자상관없이 길이도 상관없이 : +)@(앞이랑 마찬가지)
+userEmail.addEventListener("input", function () {
+  const regExp = /^[!-~]+@[!-~]+$/;
+  const check = regExp.test(userEmail.value);
+  if (check) {
+    userEmailSpan.style.color = "green";
+    userEmailSpan.innerHTML = "OK";
+  } else {
+    userEmailSpan.style.color = "red";
+    userEmailSpan.innerHTML = "이메일 형식에 맞춰서 입력하세요";
+  }
+});
